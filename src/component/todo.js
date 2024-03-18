2ec446aebbf7df4bab49fef30431d7be6595c5fb
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-03-18 13:32:54 +0500

    added all items

--- src/component/todo.js
+++ src/component/todo.js
@@ -1,15 +1,87 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
+
+import Todo2 from '../images/todo2.svg';
+
+// getting back from localStorage
+const getLocalData = () => {
+   const lists = localStorage.getItem("mytodolist");
+
+   if(lists) {
+      return JSON.parse(lists);
+   } else {
+      return [];
+   }
+};
 
 const Todo = () => {
    const [ inputData, setInputData ] = useState('');
+   const [ items, setItems ] = useState(getLocalData());
+   const [ isEditItem, setIsEditItem ] = useState('');
+   const [ toggleButton, setToggleButton ] = useState(false);
+
+   // adding items
+   const addItem = () => {
+      if(!inputData) {
+         alert("Please add Item");
+      } 
+      else if(inputData && toggleButton) {
+         setItems(
+            items.map((curElem) => {
+               if(curElem.id === isEditItem) {
+                  return { ...curElem, name: inputData };
+               }
+               return curElem;               
+            })
+         );
+         setInputData([]);
+         setIsEditItem(null);
+         setToggleButton(false);
+      }
+      else {
+         const myNewInputData = {
+            id: new Date().getTime().toString(),
+            name: inputData,
+         };
+         setItems([... items, myNewInputData]);
+         setInputData('');
+      }
+   };
+
+   // editing item
+   const editItem = (index) => {
+     const item_todo_edited = items.find((curElem) => {
+      return curElem.id === index;
+     })
+     setInputData(item_todo_edited.name);
+     setIsEditItem(index);
+     setToggleButton(true);
+   }
+
+   // deleting items
+   const deleteItem = (index) => {
+      const updatedItem = items.filter((curElem) => {
+       return curElem.id !== index;
+      });
+      setItems(updatedItem);
+   }
+
+   // removing all items
+   const removeAll = () => {
+      setItems([]);
+   }
+
+   // adding item to localStorage
+   useEffect(() => {
+      localStorage.setItem("mytodolist", JSON.stringify(items));
+   }, [items])
 
   return (
     <>
       <div className="main-div">
          <div className="child-div">
             <figure>
-               <img src="/public/img/img.png" alt="todologo" />
+               <img src={Todo2} alt="todologo" />
                <figcaption>Add Your List Here 😊</figcaption>
             </figure>
             <div className="addItems">
@@ -20,23 +92,38 @@ const Todo = () => {
                   value={inputData}
                   onChange={(e) => setInputData(e.target.value)}
                />
-               <i className="fa fa-plus add-btn"></i>
+               {toggleButton ? (
+                  <i className="far fa-edit add-btn" onClick={addItem}></i>
+                  ) : (
+                     <i className="fa fa-plus add-btn" onClick={addItem}></i>
+                     )}
             </div>
             {/* show */}
             <div className="showItems">
-               <div className="eachItem">
-                  <h3>Apple</h3>
-                  <div className="todo-btn">
-                  <i className="far fa-edit add-btn"></i>
-                  <i className="far fa-trash-alt add-btn"></i>
-                  </div>
-               </div>
+               {items.map((curElem, index) => {
+                  return (
+                     <div className="eachItem" key={curElem.id}>
+                        <h3>{ curElem.name }</h3>
+                        <div className="todo-btn">
+                           <i className="far fa-edit add-btn"
+                              onClick={() => editItem(curElem.id)}
+                           ></i>
+                           <i className="far fa-trash-alt add-btn"
+                              onClick={() => deleteItem(curElem.id)}   
+                           ></i>
+                        </div>
+                     </div>
+                  )
+               })}
             </div>
             {/* hide */}
             <div className="showItems">
-               <button className="btn effect04" data-sm-link-text="Remove All">
+               <button className="btn effect04" data-sm-link-text="Remove All"
+                  onClick={removeAll}   
+               >
                   <span>CHECK LIST</span>
                </button>
+               <p className='copy-right'> ©️ developer-rak</p>
             </div>
          </div>
       </div>
